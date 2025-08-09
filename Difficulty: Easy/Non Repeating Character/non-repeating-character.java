3861f448567592355282c9750cca6f5bdffaa03c
Sarbojit Podder <113027026+Sarbojit357@users.noreply.github.com>
2025-08-09 23:50:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,11 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+        // code hereclass Solution {
+  
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) freq[c - 'a']++;
+        for (char c : s.toCharArray()) if (freq[c - 'a'] == 1) return c;
+        return '$';
+    }
+}
+
