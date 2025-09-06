f923557a5ef59d49981d9085f81dd6cf0767aaa0
Sarbojit Podder <113027026+Sarbojit357@users.noreply.github.com>
2025-09-06 21:16:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Distinct occurrences/distinct-occurrences.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public int subseqCount(String txt, String pat) {
+        int n = txt.length(), m = pat.length();
+        int[][] dp = new int[n + 1][m + 1];
+        
+        for (int i = 0; i <= n; i++) dp[i][0] = 1; // empty pattern
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (txt.charAt(i - 1) == pat.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
